493763af4ed9c0bba1271f3a8ddf9f19b8e3f79e
rahmatsayfuddin <sayrahmat@gmail.com>
2023-05-19 07:10:44 -0400

    Landing Page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+@section('container')
+<link rel="stylesheet" href="css/landing.css">
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+      </div>
+    </header>
+
+    <main role="main" class="inner cover">
+      <h1 class="cover-heading">Aksara</h1>
+      <p class="lead">Unlock cultural appreciation through comprehensive resources and interactive tools. Join us in the fascinating world of script learning.</p>
+      <p class="lead">
+        <a href="#" class="btn btn-lg btn-secondary">Start Here</a>
+      </p>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+      <div class="inner">
+        <p>start </p>
+      </div>
+    </footer>
+  </div>
+@endsection
